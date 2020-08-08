39475d70e44911477ba3f018a4b9a079865a6374
Shahid Khan <8899379+shahidify@users.noreply.github.com>
2020-08-08 18:56:03 -0500

    Tic Tac Toe complete

--- src/App.js
+++ src/App.js
@@ -23,16 +23,51 @@ function genenrateGrid(rows, columns, mapper) {
 
 const newTicTacToeGrid = () => genenrateGrid(3, 3, () => null);
 
+function checkThree(a, b, c) {
+  if (!a || !b || !c) return false;
+  return a === b && b === c;
+}
+
+// const flatten = (arr) => Array.prototype.flat(arr);
+const flatten = (arr) => arr.reduce((acc, cur) => [...acc, ...cur], []);
+// arr.reduce((acc, cur) => [...acc, ...cur], []);
+
+function checkForWin(flatGrid) {
+  const [nw, n, ne, w, c, e, sw, s, se] = flatGrid;
+
+  return (
+    checkThree(nw, n, ne) ||
+    checkThree(w, c, e) ||
+    checkThree(sw, s, se) ||
+    checkThree(nw, w, sw) ||
+    checkThree(n, c, s) ||
+    checkThree(ne, e, se) ||
+    checkThree(nw, c, se) ||
+    checkThree(ne, c, sw)
+  );
+}
+
+function checkForDraw(flatGrid) {
+  return (
+    !checkForWin(flatGrid) &&
+    flatGrid.filter(Boolean).length === flatGrid.length
+  );
+}
+
 const NEXT_TURN = {
   O: "X",
   X: "O"
 };
 const getInitialState = () => ({
   grid: newTicTacToeGrid(),
+  status: "inProgress",
   turn: "X"
 });
 
 const reducer = (state, action) => {
+  if (state.status === "success" && action.type !== "RESET") {
+    return state;
+  }
   switch (action.type) {
     case "RESET":
       return getInitialState();
@@ -47,8 +82,15 @@ const reducer = (state, action) => {
       const nextState = clone(state);
 
       nextState.grid[y][x] = turn;
+      const flatGrid = flatten(nextState.grid);
+      if (checkForWin(flatGrid)) {
+        nextState.status = "success";
+        return nextState;
+      }
+      if (checkForDraw(flatGrid)) {
+        return getInitialState();
+      }
       nextState.turn = NEXT_TURN[turn];
-
       return nextState;
     }
     default:
@@ -58,7 +100,7 @@ const reducer = (state, action) => {
 
 function Game() {
   const [state, dispatch] = React.useReducer(reducer, getInitialState());
-  const { grid } = state;
+  const { grid, status, turn } = state;
 
   const handleClick = (x, y) => {
     dispatch({ type: "CLICK", payload: { x, y } });
@@ -69,8 +111,15 @@ function Game() {
   };
 
   return (
-    <div>
-      <div>
+    <div style={{ display: "inline-block" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between"
+        }}
+      >
+        <div>Next turn: {turn} </div>
+        <div>{status === "success" ? `${turn} won!` : null}</div>
         <button onClick={reset} type="button">
           reset
         </button>
